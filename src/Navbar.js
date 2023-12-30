263b2ce4b93c42ce4fe0cf0e149f7f581e11e625
rahulsharma0786 <77057152+rahulsharma0786@users.noreply.github.com>
2023-12-30 23:06:59 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,10 +9,15 @@ const Navbar=()=>{
 
     
     document.getElementById("demo").style.display="block"
+    document.getElementById("btnn1").style.display="none"
+    document.getElementById("btnn2").style.display="block"
   }
 
   function hide(){
     document.getElementById("demo").style.display="none"
+    document.getElementById("btnn2").style.display="none"
+    document.getElementById("btnn1").style.display="block"
+    
   }
   
   return(
@@ -27,14 +32,14 @@ const Navbar=()=>{
 <li><Link to="/project" style= { { textDecoration:'none',color:"white" }}>Project</Link></li>
 <li><Link to="/gallery" style= { { textDecoration:'none',color:"white" }}>Gallery</Link></li>
 <li><Link to="/contact" style= { { textDecoration:'none',color:"white" }}>Contact</Link></li>
-<div id="busbar">
-<img src={busbar} onClick={show} />
-<img src={cross}  style={{marginTop:"8px" ,height:"22px", width:"25px"}} onClick={hide} />
-</div>
+
 
 
 </ul>
-
+<div id="busbar">
+<img src={busbar} onClick={show} id="btnn1" />
+<img src={cross}  onClick={hide} id="btnn2" />
+</div>
 
 </nav>
 
